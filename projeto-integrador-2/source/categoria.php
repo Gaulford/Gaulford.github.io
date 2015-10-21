664dcc571ecabb5d3dc94a978927732d88014c20
William Magalhãesil.com <william.ma.ro@gmail.com>
2015-10-21 08:32:48 -0200

    Produção da tela genérica de cadastro.

--- projeto-integrador-2/source/categoria.php
+++ projeto-integrador-2/source/categoria.php
@@ -42,60 +42,76 @@
             </nav>
         </header>
 
-        <nav class="cmp-access">
-            <article class="mod-user-logged">
-                <img src="#" alt="">
-                <div>
-                    <h1>Victoria Baker</h1>
-                    <p>Santa Ana, CA</p>
-                </div>
-                <a href="#" title="Editar perfil">
-                    <i class="fa fa-power-off"></i>
-                </a>
-            </article>
+        <div class="cmp-container">
 
-            <h2>Menu principal</h2>
+            <nav class="cmp-access">
+                <article class="mod-user-logged">
+                    <a href="#" title="Emma Stone" class="user-photo">
+                        <img src="assets/img/thumb-user.jpg" alt="Emma Stone">
+                    </a>
+                    <div class="user-infos">
+                        <h1>Emma Stone</h1>
+                        <p>
+                            <i class="fa fa-map-marker"></i>
+                            Santa Ana, CA
+                        </p>
+                    </div>
+                    <a href="#" title="Sair" class="btn-sair">
+                        <i class="fa fa-power-off"></i>
+                    </a>
+                </article>
 
-            <ul>
-                <li>
-                    <a href="#" title="Home">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="#" title="Usuários">
-                        Usuários
-                    </a>
-                </li>
-                <li>
-                    <a href="#" title="Produtos">
-                        Produtos
-                    </a>
-                </li>
-                <li>
-                    <a href="#" title="Categorias">
-                        Categorias
-                    </a>
-                </li>
-            </ul>
-        </nav>
+                <h2>Menu principal</h2>
 
-        <section class="content">
-            <h1>Categoria:Nome categoria </h1>
-            <input type="text" name="name-category" placeholder="Insira o nome da categoria">
-            <textarea name="desc-categoty" cols="30" rows="10" placeholder="Insira a descrição da categoria"></textarea>
-            <button>
-                Inserir categoria
-            </button>
-        </section>
+                <ul>
+                    <li>
+                        <a href="#" title="Home">
+                            <i class="fa fa-home"></i>
+                            Home
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Usuários">
+                            <i class="fa fa-users"></i>
+                            Usuários
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Produtos">
+                            <i class="fa fa-square"></i>
+                            Produtos
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Categorias">
+                            <i class="fa fa-ticket"></i>
+                            Categorias
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
 
-        <footer class="cmp-footer">
-            <details open="open">
-                <summary>
-                    © 2015 by <a href="#" title="William Magalhães Rodrigues">William Magalhães Rodrigues</a>.
-                </summary>
-            </details>
-        </footer>
+            <section class="cmp-content">
+                <h1>Categoria:Nome categoria </h1>
+                <input type="text" name="name-category" placeholder="Insira o nome da categoria">
+                <textarea name="desc-categoty" cols="30" rows="10" placeholder="Insira a descrição da categoria"></textarea>
+                <button>
+                    Inserir categoria
+                </button>
+
+                <footer class="cmp-footer">
+                    <details open="open">
+                        <summary>
+                            © 2015 by <a href="#" title="William Magalhães Rodrigues">William Magalhães Rodrigues</a>.
+                        </summary>
+                    </details>
+                </footer>
+            </section>
+        </div>
 
         <script src="assets/js/lib/jquery/jquery.js"></script>
         <script src="assets/js/main.js"></script>
