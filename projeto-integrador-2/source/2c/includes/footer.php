d71e4663a3164e85ebbdbedfbc13d881f8d67d63
William Magalhães <william.rodrigues@wunderman.com>
2016-12-07 21:38:24 -0200

    - Removendo arquivos do repositório

--- projeto-integrador-2/source/2c/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="cmp-footer">
-    <details open="open">
-        <summary>
-            © 2015 by <a href="#" title="William Magalhães Rodrigues">Grupo Juliet PI</a>.
-        </summary>
-    </details>
-</footer>
--- projeto-integrador-2/source/2c/includes/scripts.php
+++
@@ -1,2 +0,0 @@
-<script src="assets/js/lib/jquery/jquery.js"></script>
-<script src="assets/js/main.js"></script>
